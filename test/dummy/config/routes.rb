064e18f4858f726947ee9648502540ec0d101844
Justus Daniel Eapen Jr <justuseapen@gmail.com>
2014-02-24 20:37:02 +0000

    Set routes and fixed index view. Tests green

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,7 @@
 Dummy::Application.routes.draw do
+  Dummy::Application.routes.draw do
+    get "/home", to: "home#index", as: :home
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
